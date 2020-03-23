f1d535052707b51e05ccd7886784f35f9845fa56
BSliwka <61915486+BSliwka@users.noreply.github.com>
2020-03-23 11:41:47 +0100

    Add files via upload

--- guess computer's number/source/main.cpp
+++ guess computer's number/source/main.cpp
@@ -1,5 +1,14 @@
 #include <Windows.h>
 #include "res.h"
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <time.h>
+
+using namespace std;
+
+int num;
+int i = 0;
 
 INT_PTR CALLBACK DialogProc(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
@@ -9,11 +18,7 @@ INT_PTR CALLBACK DialogProc(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lPara
       DestroyWindow(hwndDlg);
       PostQuitMessage(0);
       return true;
-  case WM_LBUTTONDOWN:
-      CHAR szText[200];
-      wsprintf(szText, "Kliknales myszka x=%d, y=%d", LOWORD(lParam), HIWORD(lParam));
-      MessageBox(hwndDlg, szText, TEXT("Klikniecie"), MB_OK);
-      return true;
+
   case WM_COMMAND:
     switch (HIWORD(wParam))
     {
@@ -21,13 +26,37 @@ INT_PTR CALLBACK DialogProc(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lPara
       switch (LOWORD(wParam))
       {
         case IDC_BUTTON1:
-         MessageBox(hwndDlg, TEXT("Test"), TEXT("Klikniecie"), MB_OK);
-         HWND hwndEditBox = GetWindowTextLength(hwndEditBox);
-       IDC_EDIT1:
+         HWND hwndEditBox = GetDlgItem(hwndDlg, IDC_EDIT1);
          int iTextLength = GetWindowTextLength(hwndEditBox);
          CHAR szText[500];
          GetWindowText(hwndEditBox, szText, iTextLength + 1);
-         SetWindowText((HWND)lParam, szText);
+         int guess = stoi(szText);
+
+         HWND textBox1 = GetDlgItem(hwndDlg, IDC_STATIC1);
+         HWND textBox2 = GetDlgItem(hwndDlg, IDC_STATIC2);
+         i++;
+         char i_buff[100];
+         sprintf_s(i_buff, "Liczba prób: %d", i);
+         SetWindowText((HWND)textBox2, i_buff);
+
+         if (guess < num)
+         {
+             SetWindowText((HWND)textBox1, "Za ma³o!");
+         }
+         else if (guess > num)
+         {
+             SetWindowText((HWND)textBox1, "Za du¿o!");
+         }
+         else
+         {
+             char w_buff[100];
+             sprintf_s(w_buff, "Wygra³eœ! (w liczbie prób: %d)", i);
+             MessageBox(0, w_buff, "Gratulacje", MB_OK);
+             DestroyWindow(hwndDlg);
+             PostQuitMessage(0);
+             return true;
+         }
+        
          return true;
       }
     }
@@ -40,6 +69,8 @@ INT_PTR CALLBACK DialogProc(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lPara
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow)
 {
+  srand((int)time(0));
+  num = rand() % 40;
   HWND hwndMainWindow = CreateDialog(hInstance, MAKEINTRESOURCE(IDD_MAINVIEW), NULL, DialogProc);
   ShowWindow(hwndMainWindow, iCmdShow);
 
--- guess computer's number/source/res.h
+++ guess computer's number/source/res.h
@@ -5,6 +5,9 @@
 #define IDD_MAINVIEW                    101
 #define IDC_BUTTON1                     1001
 #define IDC_BUTTON2                     1002
+#define IDC_EDIT1                       1005
+#define IDC_STATIC1                     1008
+#define IDC_STATIC2                     1009
 
 // Next default values for new objects
 // 
@@ -12,7 +15,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        102
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_CONTROL_VALUE         1010
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
