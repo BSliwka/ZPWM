0d656ff48b56a2b1cee6b254562fb7bb221828f1
BSliwka <61915486+BSliwka@users.noreply.github.com>
2020-03-25 15:37:31 +0100

    Add files via upload

---
+++ kajko i kokosz/source/res.h
@@ -0,0 +1,83 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by res.rc
+//
+#define IDD_MAINVIEW                    101
+#define IDD_KAJKO_VIEW                  101
+#define IDR_MENU1                       106
+#define IDR_MOJEMENU                    106
+#define IDR_KOKOSZ_MENU                 106
+#define IDD_KOKOSZ_VIEW                 109
+#define IDR_KAJKO_MENU                  110
+#define IDI_MAIN                        117
+#define IDI_KAJKO                       118
+#define IDI_KOKOSZ                      119
+#define IDC_BUTTON1                     1001
+#define IDC_BUTTON2                     1002
+#define IDC_EDIT1                       1005
+#define IDC_STATIC1                     1008
+#define IDC_STATIC2                     1009
+#define ID_MENU1_OPTION1                40001
+#define ID_MENU1_OPTION2                40002
+#define ID_MENU1_OPTION3                40003
+#define ID_APARYCJA_WYSOKI              40004
+#define ID_APARYCJA_T40005              40005
+#define ID_APARYCJA_40006               40006
+#define ID_CHARAKTER_LENIWY             40007
+#define ID_CHARAKTER_TCH40008           40008
+#define ID_CHARAKTER_PRZES40009         40009
+#define ID_CHARAKTER_DOWCIPNY           40010
+#define ID_STATYSTYKI_ZDROWIE           40011
+#define ID_ZDROWIE_G40012               40012
+#define ID_G40013                       40013
+#define ID_PRAGNIENIE_ZM40014           40014
+#define ID_STATYSTYKI_SI40015           40015
+#define ID_STATYSTYKI_ZR40016           40016
+#define ID_UZBROJENIE_MIECZ             40017
+#define ID_UZBROJENIE_TARCZA            40018
+#define ID_UZBROJENIE_40019             40019
+#define ID_APARYCJA_NISKI               40020
+#define ID_APARYCJA_SZCZUP40021         40021
+#define ID_APARYCJA_D40022              40022
+#define ID_CHARAKTER_INTELIGENTNY       40023
+#define ID_CHARAKTER_ODWA40024          40024
+#define ID_CHARAKTER_SZLACHETNY         40025
+#define ID_STATYSTYKI_ZDROWIE40026      40026
+#define ID_ZDROWIE_G40027               40027
+#define ID_G40028                       40028
+#define ID_PRAGNIENIE_ZM40029           40029
+#define ID_STATYSTYKI_SI40030           40030
+#define ID_STATYSTYKI_ZR40031           40031
+#define ID_ZDROWIE_PRAGNIENIE           40032
+#define ID_ZDROWIE_ZM40033              40033
+#define ID_ZDROWIE_PRAGNIENIE40034      40034
+#define ID_ZDROWIE_ZM40035              40035
+#define ID_HISTORIAPOSTACI_LUBI40036    40036
+#define ID_LUBI40037                    40037
+#define ID_WIERZY40038                  40038
+#define ID_NIELUBI40039                 40039
+#define ID_HISTORIAKOMIKSU_SCENARIUSZ   40040
+#define ID_INFORMACJE_HISTORIA          40041
+#define ID_HISTORIA_RODZAJ              40042
+#define ID_HISTORIA_KOMIKS              40043
+#define ID_RODZAJ_SCENARIUSZ            40044
+#define ID_RODZAJ_JANUSZCHRISTA         40045
+#define ID_RODZAJ_MACIEJKUR             40046
+#define ID_SCENARIUSZ_RYSUNKI           40047
+#define ID_SCENARIUSZ_JANUSZCHRISTA     40048
+#define ID_SCENARIUSZ_S40049            40049
+#define ID_RYSUNKI_ROKWYDANIA           40050
+#define ID_RYSUNKI_1972                 40051
+#define ID_ROKWYDANIA_TOMY              40052
+#define ID_ROKWYDANIA_14                40053
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        120
+#define _APS_NEXT_COMMAND_VALUE         40054
+#define _APS_NEXT_CONTROL_VALUE         1010
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
